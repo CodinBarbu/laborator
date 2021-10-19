34d599919723ce5abc80d8e73ab7f9f99bdd9014
Codin99 <91716481+CodinBarbu@users.noreply.github.com>
2021-10-19 22:41:55 +0300

    Adaugare tema 2

---
+++ Tema_2_Barbu_Codin/Controllers/Students.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Repositories;
+using Models;
+
+namespace Tema_2_Barbu_Codin.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        public StudentsController()
+        {
+        }
+
+        [HttpGet]
+        public IEnumerable<Student> Get()
+        {
+            return StudentsRepo.Students;
+        }
+
+        [HttpGet("{id}")]
+        public Student GetStudent(int id)
+        {
+            return StudentsRepo.Students.FirstOrDefault(student => student.Id == id);
+        }
+
+        [HttpPost]
+        public void Post([FromBody] Student student)
+        {
+            StudentsRepo.Students.Add(student);
+        }
+
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            StudentsRepo.Students.Remove(StudentsRepo.Students.FirstOrDefault(student => student.Id == id));
+        }
+    }
+}
