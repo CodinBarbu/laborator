57b5f0c6eae1f56360dedcb9086ecdef96086e25
Codin99 <91716481+CodinBarbu@users.noreply.github.com>
2021-11-12 17:22:14 +0200

    Adaugare Tema 6

---
+++ Tema_6_Barbu_Codin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tema_6_Barbu_Codin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
